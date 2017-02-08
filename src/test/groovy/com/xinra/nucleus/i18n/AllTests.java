637ffbc2b848333971090f31285eeb5b9fad155e
Erik Hofer <erik-hofer@mail.de>
2017-02-08 13:49:10 +0100

    Created gradle project.

---
+++ src/test/groovy/com/xinra/nucleus/i18n/AllTests.java
@@ -0,0 +1,18 @@
+package com.xinra.nucleus.i18n;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Test suite that runs all tests.
+ * 
+ * @author Erik Hofer
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+      
+    })
+public class AllTests {
+
+}
